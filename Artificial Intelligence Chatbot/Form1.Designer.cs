e1c92ccfe2eef18368cf2e83e6708a37a5d299cc
Efface Studios <33323258+effacestudios@users.noreply.github.com>
2018-09-30 16:50:09 +0500

    Add files via upload
    Source Code Uploaded.

---
+++ Artificial Intelligence Chatbot/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Artificial_Intelligence_Chatbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.MainLogo = new System.Windows.Forms.PictureBox();
+            this.SendBtn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SendBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // InputBox
+            // 
+            this.InputBox.BackColor = System.Drawing.Color.Black;
+            this.InputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.InputBox.Location = new System.Drawing.Point(12, 385);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(226, 20);
+            this.InputBox.TabIndex = 1;
+            this.InputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputBox_KeyDown);
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.BackColor = System.Drawing.Color.Black;
+            this.OutputBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.OutputBox.Location = new System.Drawing.Point(13, 180);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(259, 194);
+            this.OutputBox.TabIndex = 2;
+            this.OutputBox.Text = "Hi There!\r\nHow Can I Help You?";
+            this.OutputBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainLogo
+            // 
+            this.MainLogo.Image = global::Artificial_Intelligence_Chatbot.Properties.Resources._1;
+            this.MainLogo.Location = new System.Drawing.Point(12, 12);
+            this.MainLogo.Name = "MainLogo";
+            this.MainLogo.Size = new System.Drawing.Size(260, 162);
+            this.MainLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MainLogo.TabIndex = 0;
+            this.MainLogo.TabStop = false;
+            // 
+            // SendBtn
+            // 
+            this.SendBtn.Image = global::Artificial_Intelligence_Chatbot.Properties.Resources.icons8_Sent_512px;
+            this.SendBtn.Location = new System.Drawing.Point(244, 380);
+            this.SendBtn.Name = "SendBtn";
+            this.SendBtn.Size = new System.Drawing.Size(29, 30);
+            this.SendBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.SendBtn.TabIndex = 3;
+            this.SendBtn.TabStop = false;
+            this.SendBtn.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 417);
+            this.Controls.Add(this.SendBtn);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.MainLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Artificial Intelligence Bot AIML";
+            ((System.ComponentModel.ISupportInitialize)(this.MainLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SendBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainLogo;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.PictureBox SendBtn;
+    }
+}
+
---
+++ Artificial Intelligence Chatbot/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AIMLbot;
+using System.Speech;
+using System.Speech.Synthesis;
+namespace Artificial_Intelligence_Chatbot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            GetBotResponse();
+        }
+
+        public void GetBotResponse()
+        {
+            Bot AI = new Bot();
+            AI.loadSettings(); //It will Load Settings from its Config Folder with this code
+            AI.loadAIMLFromFiles(); //With this Code It Will Load AIML Files from its AIML Folder
+            AI.isAcceptingUserInput = false; //With this Code it will Disable UserInput For Now
+            User myuser = new User("Username Here", AI); //With This Code We Will Add The User Through AI/Bot
+            AI.isAcceptingUserInput = true; //Now The User Input is Enabled Again with this Code
+            Request r = new Request(InputBox.Text, myuser, AI); //With This Code it will Request The Response From AIML Folders
+            Result res = AI.Chat(r); //With This Code It Will Get Result
+            OutputBox.Text = "Efface C# Bot: " + res.Output; //With this Code It Will Write the Result of Textbox1 Response to Textbox2 text
+            //Now Coding Is Finished!
+            //Now Add/Copy & Paste AIML Folder & Config Folder to the Project Directory
+            //Now Test the Bot (Without Voice)
+
+            //Now Adding Bot Voice
+            SpeechSynthesizer reader = new SpeechSynthesizer(); //Add System.Speech Reference First In Order To Creating It.
+            reader.Speak(res.Output); //Set Reader To Response Output of AIML To Speak
+
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                GetBotResponse();
+                e.Handled = e.SuppressKeyPress = true; //This Code for Disabling Beep Sound On Enter Key
+            }
+        }
+
+      
+    }
+}
